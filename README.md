651c2757b0229b67a66c0fb9a7d76f827d1d5359
DGIII <34903375+DomGonzalesIII@users.noreply.github.com>
2020-06-14 20:56:48 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# LDTemplateFiles
-These are a set of templates for setting up a learning den page. They serve to save time and maintain consistency.
+# Colors
+Help your toddler learn to recognize colors with this simple, interactive page.
+
+Still to do:
+```
+- Add audio files and functionality
+```
--- index.js
+++ index.js
@@ -1,5 +1,56 @@
 // jshint esversion: 8
+// jscs:disable maximumLineLength
 
 /* Copyright (c) 2020 The Learning Den */
 
-alert('Click on the thing or press space to change the thing');
+// variables
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'white', 'black', 'grey', 'brown', 'pink'];
+const hexCodes = ['#FF0000', '#FF7F00', '#FFFF00', '#008000', '#0000FF', '#4B0082', '#f5f5f5', '#050505', '#333333', '#663300', '#FF69B4'];
+var currentColorIndex = 0;
+
+// set initial color name text and background color
+document.querySelector('#colorName').innerText = colors[currentColorIndex];
+document.body.style.backgroundColor = hexCodes[currentColorIndex];
+
+// proceed to next color and word when right arrow is clicked
+document.querySelector('#rang').addEventListener('click', function () {
+
+  currentColorIndex++;
+
+  // if currentColorIndex is out of range, reset it to zero
+  if (currentColorIndex >= colors.length || currentColorIndex >= hexCodes.length) {
+    currentColorIndex = 0;
+  }
+
+  // change color name text
+  document.querySelector('#colorName').innerText = colors[currentColorIndex];
+
+  // change color scheme
+  document.body.style.backgroundColor = hexCodes[currentColorIndex];
+
+});
+
+// revert to previous color and word when left arrow is clicked
+document.querySelector('#lang').addEventListener('click', function () {
+
+  currentColorIndex--;
+
+  // if currentColorIndex is out of range, reset it to zero
+  if (currentColorIndex < 0 || currentColorIndex < 0) {
+    currentColorIndex = colors.length - 1;
+  }
+
+  // change color name text
+  document.querySelector('#colorName').innerText = colors[currentColorIndex];
+
+  // change color scheme
+  document.body.style.backgroundColor = hexCodes[currentColorIndex];
+
+});
+
+// prevents highlighting of sight word if user clicks quickly (double-clicks)
+document.body.addEventListener('mousedown', function (event) {
+  if (event.detail > 1) {
+    event.preventDefault();
+  }
+}, false);
